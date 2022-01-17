b90d593f8e1d8ebb1a46a3c3314350e5ab7fb5b6
Lforwhat <1277149488@qq.com>
2022-01-17 11:52:40 +0800

    :sparkles: add includes templates, using infer

---
+++ type-challenges/Includes/template.ts
@@ -0,0 +1,3 @@
+import { Equal } from '@type-challenges/utils'
+
+type Includes<T extends readonly any[], U> = T extends [infer First, ...infer Rest] ? Equal<First, U> extends true ? true : Includes<Rest, U> : false
