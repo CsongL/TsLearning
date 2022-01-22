f34eee01dd3fb409e4c0fea7875600578bef5d1a
Lforwhat <1277149488@qq.com>
2022-01-22 11:44:36 +0800

    :flags: add comment on promise.all

--- type-challenges/Promise.all/template.ts
+++ type-challenges/Promise.all/template.ts
@@ -1,1 +1,4 @@
+// 遍历一个数组, [k in keyof T] : T[k]
+// 判断数组中元素的类型  T[k] extends Promise<infer R> ? R : T[K]
+// 因为测试用例 有 cosnt 所以需要规定 参数 readonly
 declare function PromiseAll<T extends any[]>(values: readonly [...T]) : Promise<{ [K in keyof T] : T[K] extends Promise<infer R> ? R : T[K]}>
