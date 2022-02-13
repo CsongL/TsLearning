1e17ef9c2e319d07b28a4b0e7c746c5797a42dd2
Lforwhat <1277149488@qq.com>
2022-02-13 12:20:34 +0800

    :sparkles: add InorderTraversal challenges

---
+++ type-challenges/InorderTraversal/template.ts
@@ -0,0 +1,7 @@
+interface TreeNode {
+    val: number,
+    left: TreeNode | null,
+    right: TreeNode | null
+}
+
+type InorderTraversal<T extends TreeNode | null> = T extends TreeNode ? T['left'] extends TreeNode ? [...InorderTraversal<T['left']>, T['val'], ...InorderTraversal<T['right']>] : T['right'] extends TreeNode ? [T['val'], ...InorderTraversal<T['right']>] : [T['val']] : []; 
