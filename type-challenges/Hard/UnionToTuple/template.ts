7f7ba001479f41e70408d312b007b1c188793a04
Lforwhat <1277149488@qq.com>
2022-02-23 11:26:14 +0800

    Update template.ts

--- type-challenges/Hard/UnionToTuple/template.ts
+++ type-challenges/Hard/UnionToTuple/template.ts
@@ -1,1 +1,6 @@
-type Omit<T, K> = T extends T ? T extends K ? never : T : never
+type OmitX<T, K> = T extends T ? T extends K ? never : T : never
+type UnionToTuple<T, K = T> = [T] extends [never] 
+                                ? []
+                                : T extends T
+                                    ? [T, ...UnionToTuple<OmitX<K, T>>]
+                                    : []
