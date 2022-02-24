01e7abf58a1717292e0ce84cc0085b1a2ed80fc4
Lforwhat <1277149488@qq.com>
2022-02-24 14:18:21 +0800

    Update template.ts

--- type-challenges/Hard/DeepPick/template.ts
+++ type-challenges/Hard/DeepPick/template.ts
@@ -1,1 +1,8 @@
-// 通过函数参数的形式将Union类型的type转变为 Insection类型
+// 通过函数参数的形式将Union类型的type转变为 Intersection类型
+type DeepPick<O, S> = UnionToIntersection<
+    S extends `${infer F}.${infer L}` ? {
+        [K in keyof O as K extends F ? K : never] : O[K] extends object ? DeepPick<O[K], L> : O[K]
+    } : S extends '' ? unknown : {
+        [K in keyof O as K extends S ? K : never] : O[K]
+    }
+>
